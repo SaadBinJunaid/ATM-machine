5685cbdbcb80cb6295a50cde24e16daba9b0694a
SaadBinJunaid <160081324+SaadBinJunaid@users.noreply.github.com>
2024-04-01 03:45:05 +0500

    This is a ATM machine

---
+++ index.ts
@@ -0,0 +1,50 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let myBalance = 10000; // Dollars
+let myPin = 1234;
+
+let pinAnswer = await inquirer.prompt([
+  {
+    name: "pin",
+    message: "Enter your pin",
+    type: "number",
+  },
+]);
+
+if(pinAnswer.pin === myPin){
+  console.log("Correct pin code!!!");
+     
+  let operationAns = await inquirer.prompt([
+    {
+      name: "operation",
+      message: "Please select option",
+      type: "list",
+      choices: ["Withdraw","Check balance"],
+    }
+]);
+
+console.log(operationAns);
+
+ if(operationAns.operation === "Withdraw"){
+  let amountAns = await inquirer.prompt(
+    [
+        {
+            name: "amount",
+            message: "Enter your amount",
+            type: "number",
+        }
+    ]
+  )
+  myBalance -= amountAns.amount;
+  
+  console.log("your remaining balance is " + myBalance)
+}else if (operationAns.operation === "Check balance"){
+    console.log("your balance is " + myBalance)
+}
+}
+
+else{
+    console.log("Incorrect pin number");
+}
